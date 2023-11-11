3938f7f43dc43807d0d01bb60860bce4a4095283
Joe Fry <JoeFry15@users.noreply.github.com>
2023-11-11 10:35:36 +0000

    Add buttons with listening logic

--- rps.js
+++ rps.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"];
+  const choices = ["Rock", "Paper", "Scissors"];
   const ran = Math.floor(Math.random() * choices.length);
   return choices[ran];
 }
@@ -8,46 +8,25 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return "draw";
   } else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper")
   ) {
     return "player";
   } else if (
-    (playerSelection === "rock" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "scissors") ||
-    (playerSelection === "scissors" && computerSelection === "rock")
+    (playerSelection === "Rock" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Rock")
   ) {
     return "computer";
   }
 }
 
-function game() {
-  const score = [0, 0];
-  for (let i = 0; i < 5; i++) {
-    const result = playRound(
-      prompt("What's your choice?").toLowerCase(),
-      getComputerChoice()
-    );
-    if (result === "player") {
-      score[0]++;
-    } else {
-      score[1]++;
-    }
-  }
-  if (score[0] < score[1]) {
-    console.log(
-      `The score is Player: ${score[0]}, Computer: ${score[1]}. Player has won!`
-    );
-  } else if (score[0] < score[1]) {
-    console.log(
-      `The score is Player: ${score[0]}, Computer: ${score[1]}. Computer has won!`
-    );
-  } else {
-    console.log(
-      `The score is Player: ${score[0]}, Computer: ${score[1]}. Draw!`
-    );
-  }
-}
+let btn = document.querySelectorAll("button");
 
-game();
+btn.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    const result = playRound(e.target.innerText, getComputerChoice());
+    console.log(result);
+  });
+});
